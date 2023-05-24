db4a008b86c6de6a7d2d2aa4535bf3fde5899021
NguyenHung0707 <111960391+NguyenHung0707@users.noreply.github.com>
2023-05-24 09:16:01 +0700

    abstract

---
+++ GeometricObject.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+public abstract class GeometricObject
+{
+    protected string color;
+    protected double weight;
+    protected GeometricObject()
+    {
+        color = "white";
+        weight = 1.0;
+    }
+
+    protected GeometricObject(string color, double weight)
+    {
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public string PColor
+    {
+        get { return color; }
+        set { color = value; }
+    }
+
+    public double PWeight
+    {
+        get { return weight; }
+        set { weight = value; }
+    }
+
+    public abstract double findArea();
+    public abstract double findPerimeter();
+}
+
